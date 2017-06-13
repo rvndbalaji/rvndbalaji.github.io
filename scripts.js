85a041c04d1b3c8a9831c151d215a0072b9c6c1e
Aravind Balaji <aravind.balaji@live.com>
2017-06-13 21:18:49 +0530

    Done-Part2

--- scripts.js
+++ scripts.js
@@ -6,11 +6,15 @@ google.charts.load("current", {packages:["corechart"]});
     {
         var data = google.visualization.arrayToDataTable([
           ['Skill', 'Experience'],
-          ['Java | Android', 49],
-          ['HTML | CSS | JS | SQL', 20],
-          ['Visual Basic', 16],
-          ['PHP', 8],
-          ['Python', 7],
+          ['Java | Android', 22],
+          ['C | C++', 12],
+          ['Visual Basic', 12],
+          ['HTML 5',11],
+          ['CSS 3', 11],
+          ['JavaScript', 10],
+          ['SQL', 9],
+          ['PHP', 6.5],
+          ['Python', 6.5],
         ]);
 
         var options =
@@ -37,11 +41,18 @@ google.charts.load("current", {packages:["corechart"]});
             },
           slices:
             {
-              0: { color : '#F44336', offset : 0.1 },
-              1: { color : '#4CAF50', offset : 0.075 },
-              2: { color : '#7E57C2', offset : 0.05 },
-              3: { color : '#FFEB3B', offset : 0.025 },
-              4: { color : '#2196F3', offset : 0.0 },
+              0: { color : '#EC2025', offset : 0.1 },
+              1: { color : '#607D8B', offset : 0.08 },
+              2: { color : '#AB47BC', offset : 0.07 },
+              3: { color : '#E44D26', offset : 0.06 },
+
+              4: { color : '#008DD2', offset : 0.05 },
+
+              5: { color : '#FFEB3B', offset : 0.04 },
+              6: { color : '#8BC34A', offset : 0.03 },
+              7: { color : '#9575CD', offset : 0.02 },
+              8: { color : '#4DB6AC', offset : 0.0 },
+
             },
             pieSliceTextStyle :
             {
