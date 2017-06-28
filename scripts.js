2c62c7fcdfc679d50764902d07278e1796064dcc
Aravind Balaji <aravind.balaji@live.com>
2017-06-28 18:14:32 +0530

    Beautified

--- scripts.js
+++ scripts.js
@@ -1,65 +1,60 @@
     var text = "Coder. Designer. Enthusiast";
 
-    var type = function(component,message,index,interval)
-    {
+    var type = function (component, message, index, interval) {
 
-        if(index < message.length)
-            {
-                $(component).append(message[index++]);
-                setTimeout(function()
-                {
-                   type(component,message,index,interval)
-                },interval);
-            }
+        if (index < message.length) {
+            $(component).append(message[index++]);
+            setTimeout(function () {
+                type(component, message, index, interval)
+            }, interval);
+        }
     }
 
-$(document).ready(function()
-{
+    $(document).ready(function () {
 
-  //1. Animate typing effect
-      type("#my_subtitle",text,-1,50);
-     setInterval(function()
-                {
-                    $("#my_subtitle").text("> ");
-                    type("#my_subtitle",text,-1,50);
-                },5000);
+        //1. Animate typing effect
+        type("#my_subtitle", text, -1, 50);
+        setInterval(function () {
+            $("#my_subtitle").text("> ");
+            type("#my_subtitle", text, -1, 50);
+        }, 5000);
 
 
-  //2.Smooth scroll
-      $('a').click(function()
-                   {
-                        $('html, body').animate(
-                            {
-                                scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-                            }, 1000);
-                    return false;
-                  });
+        //2.Smooth scroll
+        $('a').click(function () {
+            $('html, body').animate({
+                scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+            }, 1000);
+            return false;
+        });
 
-});
+    });
 
-$(function(){
+    $(function () {
 
-	var $window = $(window);		//Window object
+        var $window = $(window); //Window object
 
-	var scrollTime = 1;			//Scroll time
-	var scrollDistance = $(window).height();		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+        var scrollTime = 1; //Scroll time
+        var scrollDistance = $(window).height(); //Distance. Use smaller value for shorter scroll and greater value for longer scroll
 
-	$window.on("mousewheel DOMMouseScroll", function(event){
+        $window.on("mousewheel DOMMouseScroll", function (event) {
 
-		event.preventDefault();
+            event.preventDefault();
 
-		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-		var scrollTop = $window.scrollTop();
-		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+            var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+            var scrollTop = $window.scrollTop();
+            var finalScroll = scrollTop - parseInt(delta * scrollDistance);
 
-		TweenMax.to($window, scrollTime, {
-			scrollTo : { y: finalScroll, autoKill:true },
-				ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
-				autoKill: true,
-				overwrite: 5
-			});
+            TweenMax.to($window, scrollTime, {
+                scrollTo: {
+                    y: finalScroll,
+                    autoKill: true
+                },
+                ease: Power1.easeOut, //For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+                autoKill: true,
+                overwrite: 5
+            });
 
-	});
+        });
 
-});
-
+    });
