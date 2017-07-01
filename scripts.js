182f9b0d18314a21d7540973a30358e5690b4fb0
Aravind Balaji <aravind.balaji@live.com>
2017-07-01 19:07:39 +0530

    Animations

--- scripts.js
+++ scripts.js
@@ -1,60 +1,143 @@
-    var text = "Coder. Designer. Enthusiast";
+//This function is the typer fucntion which prints a character one by one by taking a string and delay as input
+var type = function (component, message, index, interval) {
 
-    var type = function (component, message, index, interval) {
-
-        if (index < message.length) {
-            $(component).append(message[index++]);
-            setTimeout(function () {
-                type(component, message, index, interval)
-            }, interval);
-        }
+    if (index < message.length) {
+        $(component).append(message[index++]);
+        setTimeout(function () {
+            type(component, message, index, interval)
+        }, interval);
     }
+}
 
-    $(document).ready(function () {
+//This function when called proceeds to load the actual webpage after the loading animation is finished!
+var load_page = function () {
 
-        //1. Animate typing effect
-        type("#my_subtitle", text, -1, 50);
-        setInterval(function () {
-            $("#my_subtitle").text("> ");
-            type("#my_subtitle", text, -1, 50);
-        }, 5000);
+    //This function is executed after the loading is done. It hides the LOAD_SCREEN and shows the FULL_PAGE
+    setTimeout(function () {
+
+        $(".LOAD_SCREEN").animate({
+            opacity: 0.0
+        }, 1000).css('display', 'none');
 
 
-        //2.Smooth scroll
-        $('a').click(function () {
-            $('html, body').animate({
-                scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+        $(".ENTIRE_PAGE, .menu")
+            .css('display', 'block')
+            .animate({
+                opacity: 1.0
             }, 1000);
-            return false;
+    }, 1000);
+
+
+
+    //This function registers a permanant Async task that constantly keeps performing the typing animation that was earlier defined
+    //in loading screen. Its redundant, but I was unable to give a unique function without global variables
+    var rep = 0;
+    setInterval(function () {
+        $("#my_subtitle").text("> ");
+        var text = "";
+        switch (rep) {
+            case 1:
+                rep = 1;
+                text = "Designer.";
+                break;
+            case 2:
+                rep = 2;
+                text = "Enthusiast.";
+                break;
+            default:
+                rep = 0;
+                text = "Coder.";
+        }
+
+        type("#my_subtitle", text, -1, 40);
+        rep = (rep + 1) % 3;
+    }, 2000);
+
+
+    //End of Page Load Function
+}
+
+
+
+//This anonymous function registers an onMouseScroll Event Listener and overrides the default scroll
+$(function () {
+
+    var $window = $(window); //Window object
+
+    var scrollTime = 1; //Scroll time
+    var scrollDistance = $(window).height(); //Distance. Use smaller value for shorter scroll and greater value for longer scroll
+
+    $window.on("mousewheel DOMMouseScroll", function (event) {
+
+        event.preventDefault();
+
+        var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+        var scrollTop = $window.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+
+        TweenMax.to($window, scrollTime, {
+            scrollTo: {
+                y: finalScroll,
+                autoKill: true
+            },
+            ease: Power1.easeOut, //For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+            autoKill: true,
+            overwrite: 5
         });
 
     });
 
-    $(function () {
 
-        var $window = $(window); //Window object
+});
 
-        var scrollTime = 1; //Scroll time
-        var scrollDistance = $(window).height(); //Distance. Use smaller value for shorter scroll and greater value for longer scroll
+//This is the first functio nthat is ever called. It executes and animates the Load Screen, and implicitly calls load_page()
+//So until the animation is complete the page isn't loaded
+var loadscreen_animate = function () {
+    var rep = 0;
+    var exit = 0;
 
-        $window.on("mousewheel DOMMouseScroll", function (event) {
+    var loadtyper = setInterval(function () {
+        if (exit) {
+            clearInterval(loadtyper);
+            load_page();
+            return;
+        }
+        $("#load_my_subtitle").text("> ");
+        var text = "";
+        switch (rep) {
+            case 1:
+                rep = 1;
+                text = "Designer.";
+                break;
+            case 2:
+                rep = 2;
+                text = "Enthusiast.";
+                exit = 1;
+                break;
+            default:
+                rep = 0;
+                text = "Coder.";
+        }
 
-            event.preventDefault();
+        type("#load_my_subtitle", text, -1, 40);
+        rep = (rep + 1) % 3;
+    }, 2000);
 
-            var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
-            var scrollTop = $window.scrollTop();
-            var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+}
 
-            TweenMax.to($window, scrollTime, {
-                scrollTo: {
-                    y: finalScroll,
-                    autoKill: true
-                },
-                ease: Power1.easeOut, //For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
-                autoKill: true,
-                overwrite: 5
-            });
+loadscreen_animate();
+//This function is called when the page finishes loading
+$(document).ready(function () {
 
-        });
 
+    //2.Smooth scroll
+    $('a').click(function () {
+        $('html, body').animate({
+            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+        }, 1000);
+        return false;
     });
+
+
+
+});
